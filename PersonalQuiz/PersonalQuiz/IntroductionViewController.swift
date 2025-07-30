266de2124cdde0cdcbff953e54721fe956e4a503
Bhumika <bhumika0230.be23@chitkara.edu.in>
2025-07-30 17:16:29 +0530

    Personality Quiz
    File added

---
+++ PersonalQuiz/PersonalQuiz/IntroductionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Student on 29/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindTOQuizIntroduction(segue: UIStoryboardSegue){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
